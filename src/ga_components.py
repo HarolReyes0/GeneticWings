eabe8788d3e5afe174028ae631a72b7e524095bc
Harol Reyes <harolreyes0@gmail.com>
2025-01-07 22:36:54 -0400

    create_genotype method finished.

---
+++ src/ga_components.py
@@ -0,0 +1,38 @@
+import random 
+
+
+
+
+class Individual:
+    def __init__(self, genotype = ''):
+        self._genotype = genotype
+        self.__fitness_score = []
+
+    def _create_genotype(self):
+        """
+            Randomly creates the ecuation that will be use as the genotype.
+
+            Inputs:
+                None
+            Returns:
+                None  
+        """
+        n_terms = random.randint(1, 100)
+
+        # Randomly building the the ecuation that'll be use as genotype.
+        for _ in range(n_terms):
+            constant = random.randint(-100, 100)
+            operator = random.choice(['-', '+'])
+            term = f'{operator}{constant}x'
+
+            self._genotype += term
+
+
+    def _calculate_fitness(self):
+        pass
+
+    def _mutate(self):
+        pass
+
+    def get_genotype(self):
+        pass
